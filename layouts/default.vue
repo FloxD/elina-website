7cb2488a4d77cc550ca8a325830605e61a8cfd5d
FloxD <floxd@proton.me>
2022-06-05 22:34:01 +0200

    using bulma for styling and making the website and shit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main-wrapper">
+    <div class="container">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  height: 100%;
+  background-image: url("/img/background2.jpg");
+  background-size: cover;
+}
+
+#main-wrapper {
+  padding-top: 6rem;
+  //$test: #123;
+  //background-color: $test;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="is-flex is-justify-content-center">
+      <figure class="image is-200x200">
+        <img class="is-rounded" src="/img/avatar.png" alt="elina profile image">
+      </figure>
+    </div>
+    <div class="columns is-multiline">
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth"
+           href="https://www.twitch.tv/elina"
+           target="_blank">
+          Twitch
+        </a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth"
+           href="https://twitter.com/YaBoiElina"
+           target="_blank">
+          Twitter
+        </a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth"
+           href="https://www.youtube.com/channel/UCZBRLTVr1ZITpLsRGC-saGA"
+           target="_blank">
+          Youtube
+        </a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth"
+           href="https://www.tiktok.com/@elinapeepee"
+           target="_blank">
+          TikTok
+        </a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth"
+           href="https://www.instagram.com/yaboielina/"
+           target="_blank">
+          Instagram
+        </a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth">etc</a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth">etc</a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth">etc</a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth">etc</a>
+      </div>
+      <div class="column is-half">
+        <a class="button is-primary is-large is-fullwidth">etc</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  border-radius: 100px;
+}
+
+.is-200x200 {
+  width: 200px;
+  height: 200px;
+}
+
+.is-primary {
+  //background-color: #8f8749;
+  //background-color: #8d5848;
+  //background-color: #7d8463;
+  //background-color: #60664c;
+  background-color: #919973;
+}
+
+.is-primary:hover {
+  background-color: #858c69;
+}
+</style>

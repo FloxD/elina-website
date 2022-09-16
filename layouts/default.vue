83101204b16ec3658490bcda0c9cdff2f2b28d3c
FloxD <>
2022-09-16 15:14:35 +0200

    added favicon and some meta tags also for embedding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="main-wrapper">
+    <Head>
+      <Title>Links to Elina's Socials</Title>
+      <Meta name="title" content="Links to Elina's Socials"/>
+      <Meta name="description" content="This is a collection of links to Elina's social media accounts."/>
+      <Meta property="og:site_name" content="Elina's Socials"/>
+      <Meta property="og:title" content="Elina's Socials"/>
+      <Meta property="og:image" content="https://elina.chat/img/avatar4.jpg"/>
+      <Meta property="og:description" content="Here you can find links to all of Elina's social media accounts"/>
+      <Meta property="og:type" content="website"/>
+      <Meta property="og:url" content="https://elina.chat" />
+      <Meta property="twitter:card" content="summary_large_image" />
+      <Meta property="twitter:title" content="Elina's Socials" />
+      <Meta property="twitter:description" content="Here you can find links to all of Elina's social media accounts"/>
+      <Meta property="twitter:url" content="https://elina.chat" />
+      <Meta property="twitter:image" content="https://elina.chat/img/avatar4.jpg"/>
+    </Head>
     <div class="container">
       <slot/>
     </div>

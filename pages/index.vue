b2e0811aeeefe9d372666e2eaf643dc86b120ecc
FloxD <>
2023-08-28 20:03:00 +0200

    added passes

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@ const {
   error,
 } = await useFetch<ScheduleDayUtc[]>(
   //'http://localhost:8080/' + getScheduleSubdirectory() + 'get_times_utc'
-  'http://schedule.floxd.com/' + getScheduleSubdirectory() +'get_times_utc'
+  'https://schedule.floxd.com/' + getScheduleSubdirectory() +'get_times_utc'
 );
 
 if (error) {
@@ -216,6 +216,21 @@ if (error) {
       <div class="column is-half">
         <a
           class="button is-primary is-large is-fullwidth"
+          href="https://www.passes.com/elina"
+          target="_blank"
+        >
+          <svg style="width: 2rem; height: 1.5rem">
+            <path
+              fill="white"
+              d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z"
+            />
+          </svg>
+          Passes
+        </a>
+      </div>
+      <div class="column is-half">
+        <a
+          class="button is-primary is-large is-fullwidth"
           href="https://streamelements.com/elina/tip"
           target="_blank"
         >
@@ -238,7 +253,7 @@ if (error) {
       </div>
       <div>
         <img
-            :src="'http://schedule.floxd.com/' + getScheduleSubdirectory() + 'schedule.jpg'"
+            :src="'https://schedule.floxd.com/' + getScheduleSubdirectory() + 'schedule.jpg'"
             alt="Elina's streaming schedule "
         />
       </div>
